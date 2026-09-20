309729309da3c50e604630308de0748085ca2020
quadflow <quadflow@proton.me>
2026-09-20 15:11:44 -0300

    fix: raise KytosPIDInitException to be properly logged on controller start

--- kytos/core/controller.py
+++ kytos/core/controller.py
@@ -45,7 +45,8 @@ from kytos.core.dead_letter import DeadLetter
 from kytos.core.events import KytosEvent
 from kytos.core.exceptions import (KytosAPMInitException, KytosDBInitException,
                                    KytosDuplicatedSwitch,
-                                   KytosNAppSetupException)
+                                   KytosNAppSetupException,
+                                   KytosPIDInitException)
 from kytos.core.helpers import executors, now
 from kytos.core.interface import Interface
 from kytos.core.link import Link
@@ -287,7 +288,8 @@ class Controller:
             if not restart:
                 self.create_pidfile()
             await self.start_controller()
-        except (KytosDBInitException, KytosAPMInitException) as exc:
+        except (KytosDBInitException, KytosAPMInitException,
+                KytosPIDInitException) as exc:
             message = f"Kytos couldn't start because of {str(exc)}"
             self.log.error(message)
             LogManager.drain_and_stop(self.options.logmanager_drain_timeout)
@@ -320,7 +322,10 @@ class Controller:
             qmonitor.stop()
 
     def create_pidfile(self):
-        """Create a pidfile."""
+        """Create a pidfile.
+
+        Raises KytosPIDInitException for unrecoverable PID issues
+        """
         pid = os.getpid()
 
         # Creates directory if it doesn't exist
@@ -356,14 +361,17 @@ class Controller:
                 # Otherwise, overwrite the file and proceed.
                 error_msg = ("PID file {} exists. Delete it if Kytos is not "
                              "running. Aborting.")
-                sys.exit(error_msg.format(self.options.pidfile))
+                error_msg = error_msg.format(self.options.pidfile)
+                raise KytosPIDInitException(error_msg)
             except OSError:
                 try:
                     pidfile = open(self.options.pidfile, mode='w',
                                    encoding="utf8")
                 except OSError as exception:
                     error_msg = "Failed to create pidfile {}: {}."
-                    sys.exit(error_msg.format(self.options.pidfile, exception))
+                    error_msg = error_msg.format(self.options.pidfile,
+                                                 exception)
+                    raise KytosPIDInitException(error_msg)
 
         # Identifies the process that created the pidfile.
         pidfile.write(str(pid))
